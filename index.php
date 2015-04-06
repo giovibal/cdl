ec1a81690d131ed697993f7e06bc0cab14e20f55
Giovanni Baleani <giovibal@gmail.com>
2015-04-06 15:08:05 +0200

    Tolto redirect col dominio

--- index.php
+++ index.php
@@ -3,22 +3,22 @@ $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
 switch ($lang){
     case "de":
         //echo "PAGE DE";
-        header("Location: http://www.casadilavanda.com/de/");
+        header("Location: /de/");
 		exit;
         break;
     case "it":
         //echo "PAGE IT";
-        header("Location: http://www.casadilavanda.com/en/");
+        header("Location: /en/");
 		exit;
         break;
     case "en":
         //echo "PAGE EN";
-        header("Location: http://www.casadilavanda.com/en/");
+        header("Location: /en/");
 		exit;
         break;        
     default:
         //echo "PAGE EN - Setting Default";
-        header("Location: http://www.casadilavanda.com/en/");
+        header("Location: /en/");
 		exit;
         break;
 }
